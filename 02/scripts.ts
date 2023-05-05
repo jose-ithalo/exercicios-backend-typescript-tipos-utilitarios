79d789d634b39f53bb52758acffa39aa69cc6810
José Ithalo <jose_ithalo@hotmail.com>
2023-05-05 16:30:45 -0300

    Utilização de utilitários para correção

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,42 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type EnderecoCliente = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type NovoCarrinho = Omit<Carrinho, 'tipoTransacao'> &
+{
+    endereco: EnderecoCliente,
+    tipoTransacao: Lowercase<'credito' | 'Debito'>
+};
+
+const venda: NovoCarrinho = {
+    item: {
+        nome: 'string',
+        descricao: 'string',
+        valor: 120
+    },
+    qtd: 3,
+    desconto: 40,
+    frete: 57,
+    tipoTransacao: 'debito',
+    cartao: {
+        numero: 1555884,
+        validade: 'string',
+        nome: 'string',
+        cvv: 478
+    },
+    endereco: {
+        cep: 'string',
+        rua: 'string',
+        bairro: 'string',
+        cidade: 'string',
+        estado: 'string'
+    }
+}
